528c6e07c699d8e5d6b4bbb92e13ef214e05e2c4
youwaymo <youwaymo10@gmail.com>
2024-05-04 23:55:48 +0100

    fix server 1

--- module/server_data_handler.py
+++ module/server_data_handler.py
@@ -1,7 +1,11 @@
 import warnings
-
 # to ignore all warnings
 warnings.filterwarnings('ignore')
+
+import os
+
+# import the needed modules from jinja2
+from jinja2 import Environment, FileSystemLoader, select_autoescape
 
 from http.server import BaseHTTPRequestHandler, HTTPServer
 import cgi
@@ -11,12 +15,46 @@ class handler(BaseHTTPRequestHandler):
 
     # description of how to handle GET requests
     def do_GET(self):
-        self.send_response(200)
-        self.send_header('Content-type', 'text/html')
-        self.end_headers()
+        if self.path.endswith(('.css', '.js', '.png', '.woff2', '.ttf')):
+            # Serving CSS, JS, webfonts and image files
+            # os.path.splitext() splits the pathname self.path into a tuple (root, extention)
+            file_extension = os.path.splitext(self.path)[1]
 
-        message = 'salam Youssef'
-        self.wfile.write(bytes(message, 'utf8'))
+            # get the content type of the file being sent otherwise send 'application/octet-stream' 
+            # that it means to send an unkonwn file type to the browser and tell it not interpret it
+            content_type = {
+                '.css': 'text/css',
+                '.js': 'text/javascript',
+                '.png': 'image/png',
+                '.woff2': 'font/woff2',
+                'ttf': 'font/ttf'
+            }.get(file_extension, 'application/octet-stream')
+
+            self.send_response(200)
+            self.send_header('Content-type', content_type)
+            self.end_headers()
+
+            try:
+                with open('.' + self.path, 'rb') as file:
+                    self.wfile.write(file.read())
+            except FileNotFoundError:
+                self.send_response(404)
+                self.send_error(404, f'File Not Found: {self.path}')
+
+        else:
+            self.send_response(200)
+            self.send_header('Content-type', 'text/html')
+            self.end_headers()
+
+            # set the environment variable for basic configuration
+                # This configures autoescaping for the templates. 
+                # Autoescaping helps prevent XSS (cross-site scripting) attacks by automatically escaping potentially dangerous characters in the rendered output.
+            env = Environment(loader=FileSystemLoader("template"), autoescape=select_autoescape()) 
+
+            # get the template from the env variable
+            template = env.get_template('index.html')
+
+            self.wfile.write(bytes(template.render(), 'utf8'))
     
     # description of how to handle POST requests
     def do_POST(self):
